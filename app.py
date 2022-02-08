576b0aea1c85318fd6d826a1011dc4c28fa6a103
Anirban Pranto <anirbanpranto@gmail.com>
2022-02-08 16:27:30 +0800

    feat : question 1

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ import numpy as np
 
 dataframe = pd.read_csv('dataset.csv')
 
-df = pd.read_excel('../data_dictionary.xlsx')
+st.set_page_config(page_title='Covid 19 in Malaysia', page_icon=None, layout="wide",
+                   initial_sidebar_state="auto", menu_items=None)
+
+st.title("A comprehensive study of Covid 19 cases in Malaysia")
+
+st.write("The Dataset we're working with")
+#showing our initial dataframe here
+dataframe
+
+st.write('## Questions and answers')
+
+## Question 1
+
+df = pd.read_excel('data_dictionary.xlsx')
 dictionary = df.set_index('variable').T.to_dict()
 
 for key, value in dictionary.items():
@@ -18,14 +31,9 @@ for key, value in dictionary.items():
     dataframe[key]= dataframe[key].astype(str)
     dataframe.replace({key: dictionary[key]}, inplace=True)
 
-st.set_page_config(page_title='Covid 19 in Malaysia', page_icon=None, layout="wide",
-                   initial_sidebar_state="auto", menu_items=None)
+st.write('### 1. Refer to data_dictionary.csv and map the attributes values accordingly.')
+st.write('Below is the Dataframe with mapped attributes according to the data_dictionary.csv')
+st.write(dataframe)
 
-st.title("A comprehensive study of Covid 19 cases in Malaysia")
-
-st.write("The Dataset we're working with")
-#showing our initial dataframe here
-dataframe
-
-st.write('## Questions and answers')
+##
 
