f5a2eee6be1a3880dfd56882255ce70ed9bf3988
Anirban Pranto <anirbanpranto@gmail.com>
2022-02-08 17:29:48 +0800

    fix : separated the analysis from streamlit into jupyter notebook

--- app.py
+++ app.py
@@ -16,24 +16,10 @@ dataframe
 st.write('## Questions and answers')
 
 ## Question 1
-
-df = pd.read_excel('data_dictionary.xlsx')
-dictionary = df.set_index('variable').T.to_dict()
-
-for key, value in dictionary.items():
-    for i in value.values():
-        res = dict(item.split("=") for item in i.split(", "))
-        dictionary[key] = res
-
-dictionary = {outer_k.upper(): {inner_k.replace(' ', ''): inner_v for inner_k, inner_v in outer_v.items()} for outer_k, outer_v in dictionary.items()}
-
-for key, value in dictionary.items():
-    dataframe[key]= dataframe[key].astype(str)
-    dataframe.replace({key: dictionary[key]}, inplace=True)
-
 st.write('### 1. Refer to data_dictionary.csv and map the attributes values accordingly.')
 st.write('Below is the Dataframe with mapped attributes according to the data_dictionary.csv')
-st.write(dataframe)
 
+q1 = pd.read_csv('q1.csv')
+q1
 ##Question 2
 
